2337e8f1de269ac186328c9f06f754acd74477a2
Saloni Chauhan <schauhan1@hawk.iit.edu>
2024-01-28 22:10:09 -0700

    first phase of valentine website

---
+++ src/app/valentine/valentine.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-valentine',
+  templateUrl: './valentine.component.html',
+  styleUrls: ['./valentine.component.css']
+})
+export class ValentineComponent {
+
+}
