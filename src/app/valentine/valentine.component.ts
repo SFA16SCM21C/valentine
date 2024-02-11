b8208607b75fda079ec087ff0917d173e7569600
Saloni Chauhan <schauhan1@hawk.iit.edu>
2024-02-11 16:03:35 -0700

    anotheåremoved toggle

--- src/app/valentine/valentine.component.ts
+++ src/app/valentine/valentine.component.ts
@@ -10,7 +10,6 @@ export class ValentineComponent {
   header: string = "Will you be my valentine?";
   message: string = "You are mine now!";
   youAreMine: boolean = false;
-  toggleNoButton: boolean = false;
 
   onYesClick() {
     this.youAreMine = true;
